345fb955b1c484bd27cec767fe552482094b1bf4
PooPmeep <112079900+NerdyPuzzle@users.noreply.github.com>
2024-02-29 00:55:53 +0200

    fix

--- src/main/java/net/nerdypuzzle/forgemixins/Launcher.java
+++ src/main/java/net/nerdypuzzle/forgemixins/Launcher.java
@@ -2,20 +2,18 @@ package net.nerdypuzzle.forgemixins;
 
 import freemarker.template.Template;
 import net.mcreator.generator.Generator;
-import net.mcreator.generator.template.InlineTemplatesHandler;
 import net.mcreator.generator.template.base.BaseDataModelProvider;
+import net.mcreator.generator.template.base.DefaultFreemarkerConfiguration;
 import net.mcreator.io.FileIO;
 import net.mcreator.plugin.JavaPlugin;
 import net.mcreator.plugin.Plugin;
 import net.mcreator.plugin.PluginLoader;
 import net.mcreator.plugin.events.workspace.MCreatorLoadedEvent;
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
@@ -35,6 +33,7 @@ public class Launcher extends JavaPlugin {
 			if (currentGenerator != null) {
 				Set<String> fileNames = PluginLoader.INSTANCE.getResourcesInPackage(currentGenerator.getGeneratorName() + ".workspacebase");
 				Map<String, Object> dataModel = (new BaseDataModelProvider(event.getMCreator().getWorkspace().getGenerator())).provide();
+				DefaultFreemarkerConfiguration configuration = new DefaultFreemarkerConfiguration();
 				Iterator var4 = fileNames.iterator();
 
 				while(var4.hasNext()) {
@@ -44,9 +43,9 @@ public class Launcher extends JavaPlugin {
 						File generatorFile = new File(event.getMCreator().getWorkspace().getWorkspaceFolder(), file.replace(currentGenerator.getGeneratorName() + "/workspacebase", ""));
 						try {
 							String contents = IOUtils.toString(stream, StandardCharsets.UTF_8);
-							Template freemarkerTemplate = InlineTemplatesHandler.getTemplate(contents);
+							Template freemarkerTemplate = new Template("", contents, configuration);
 							StringWriter stringWriter = new StringWriter();
-							freemarkerTemplate.process(dataModel, stringWriter, InlineTemplatesHandler.getConfiguration().getObjectWrapper());
+							freemarkerTemplate.process(dataModel, stringWriter, configuration.getBeansWrapper());
 							FileIO.writeStringToFile(stringWriter.getBuffer().toString(), generatorFile);
 						} catch (Exception e) {
 							e.printStackTrace();
